5f7f13394462b59096b857a39f0204d8ba174caf
toladele <toladele99@gmail.com>
2020-05-08 18:36:42 -0400

    Simplify code, create getDice method

--- lib/main.dart
+++ lib/main.dart
@@ -26,6 +26,13 @@ class _DicePageState extends State<DicePage> {
   int leftDiceNumber = 1;
   int rightDiceNumber = 2;
 
+  void getDice() {
+    setState(() {
+      leftDiceNumber = Random().nextInt(6) + 1;
+      rightDiceNumber = Random().nextInt(6) + 1;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Center(
@@ -34,9 +41,7 @@ class _DicePageState extends State<DicePage> {
           Expanded(
             child: FlatButton(
               onPressed: () {
-                setState(() {
-                  leftDiceNumber = Random().nextInt(6) + 1;
-                });
+                getDice();
               },
               child: Image.asset('images/dice$leftDiceNumber.png'),
             ),
@@ -44,9 +49,7 @@ class _DicePageState extends State<DicePage> {
           Expanded(
             child: FlatButton(
               onPressed: () {
-                setState(() {
-                  rightDiceNumber = Random().nextInt(6) + 1;
-                });
+                getDice();
               },
               child: Image.asset('images/dice$rightDiceNumber.png'),
             ),
